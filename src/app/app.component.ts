c389687c1b998a31914da5f88ed60cc3daa26df3
Héctor <hector.vinas@institutvidreres.cat>
2024-03-21 12:10:28 +0100

    Commimimjim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'UF1ExHectorVinas';
+  constructor(private http: HttpClient) {
+
+     this.http.post('http://localhost:3020/llegirImatgesVinasHidalgo',{}).subscribe(response =>{
+       }, error =>{
+         console.log('Error: ', error);
+     });
+     /*
+     this.http.put('http://localhost:3020/copiaFitxerVinasHidalgo',{}).subscribe(response =>{
+     }, error =>{
+       console.log('Error: ', error);
+     });
+
+     this.http.get('http://localhost:3020/mostraNomsVinasHidalgo',{}).subscribe(response =>{
+     }, error =>{
+       console.log('Error: ', error);
+     });
+     */
+  }
 }
+
